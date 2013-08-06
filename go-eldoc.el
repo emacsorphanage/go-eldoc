9278390ae1bdf966da0a72cf62c98191e1725bac
Syohei YOSHIDA <syohex@gmail.com>
2013-08-06 23:58:57 +0900

    Support built in functions

--- go-eldoc.el
+++ go-eldoc.el
@@ -43,6 +43,20 @@
   :group 'go
   :prefix "go-eldoc-")
 
+(defvar go-eldoc--builtins
+  '(("append"  . "append,,func(slice []Type, elems ...Type) []Type")
+    ("close"   . "close,,func(c chan<- Type)")
+    ("delete"  . "delete,,func(m map[Type]Type1, key Type)")
+    ("panic"   . "panic,,func(v interface{})")
+    ("recover" . "recover,,func() interface{}")
+    ("complex" . "complex,,func(r, i FloatType) ComplexType")
+    ("imag"    . "imag,,func(c ComplexType) FloatType")
+    ("real"    . "real,,func(c ComplexType) FloatType")
+    ("new"     . "new,,func(Type) *Type")
+    ("cap"     . "cap,,func(v Type) int")
+    ("copy"    .  "copy,,func(dst, src []Type) int")
+    ("len"     . "len,,func(v Type) int")))
+
 (defun go-eldoc--current-arg-index (curpoint)
   (save-excursion
     (let ((count 1)
@@ -74,11 +88,22 @@
         (goto-char from)
         (re-search-forward "\\<func\\s-*(" func-start t)))))
 
-(defun go-eldoc--match-candidates (candidates cur-symbol)
+(defun go-eldoc--search-builtin-functions (symbol curpoint)
+  (if (string= symbol "make")
+      (let ((index (go-eldoc--current-arg-index curpoint)))
+        (if (= index 3)
+            "make,,func(Type, size IntegerType, capacity IntegerType) Type"
+          "make,,func(Type, size IntegerType) Type"))
+    (assoc-default symbol go-eldoc--builtins)))
+
+(defun go-eldoc--match-candidates (candidates cur-symbol curpoint)
   (when (and candidates (stringp candidates))
-    (let ((regexp (format "^\\(%s,,\\(?:func\\|type\\).+\\)$" cur-symbol)))
-      (when (string-match regexp candidates)
-        (match-string-no-properties 1 candidates)))))
+    (let* ((cands (if (string= candidates "")
+                      (go-eldoc--search-builtin-functions cur-symbol curpoint)
+                    candidates))
+           (regexp (format "^\\(%s,,\\(?:func\\|type\\).+\\)$" cur-symbol)))
+      (when (and cands (string-match regexp cands))
+        (match-string-no-properties 1 cands)))))
 
 (defun go-eldoc--begining-of-funcall-p ()
   (and (= (char-after) ?\()
@@ -109,7 +134,8 @@
         (when (and (go-eldoc--inside-funcall-p (1- (point)) curpoint)
                    (not (go-eldoc--inside-anon-function-p (1- (point)) curpoint)))
           (let ((matched (go-eldoc--match-candidates
-                          (ac-go-invoke-autocomplete) (thing-at-point 'symbol))))
+                          (ac-go-invoke-autocomplete) (thing-at-point 'symbol)
+                          curpoint)))
             (when (and matched
                        (string-match "\\`\\(.+?\\),,\\(.+\\)$" matched))
               (list :name (match-string-no-properties 1 matched)
