a665a4f62ba1659b80dc463e2b750da59d86a59b
Syohei YOSHIDA <syohex@gmail.com>
2014-03-08 10:09:43 +0900

    Update requirement cl-lib version

--- go-eldoc.el
+++ go-eldoc.el
@@ -5,7 +5,7 @@
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-go-eldoc
 ;; Version: 0.13
-;; Package-Requires: ((go-mode "0") (cl-lib "0.4"))
+;; Package-Requires: ((go-mode "0") (cl-lib "0.5"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
