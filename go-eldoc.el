2b38d64b607deacbc33c357392b4be059b2da435
Syohei YOSHIDA <syohex@gmail.com>
2014-05-30 20:07:26 +0900

    Bump up version and update Changes

--- go-eldoc.el
+++ go-eldoc.el
@@ -4,7 +4,7 @@
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-go-eldoc
-;; Version: 0.15
+;; Version: 0.16
 ;; Package-Requires: ((go-mode "0") (cl-lib "0.5"))
 
 ;; This program is free software; you can redistribute it and/or modify
