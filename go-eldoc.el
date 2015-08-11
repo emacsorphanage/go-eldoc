7d564828ed9538ca59ebab39ec503d6458657674
Syohei YOSHIDA <syohex@gmail.com>
2015-08-11 11:56:13 +0900

    Use init.el instead of .emacs
    .emacs is too old. We should use init.el since Emacs 24

--- go-eldoc.el
+++ go-eldoc.el
@@ -22,7 +22,7 @@
 
 ;;; Commentary:
 
-;; To use this package, add these lines to your .emacs file:
+;; To use this package, add these lines to your init.el file:
 ;;
 ;;     (require 'go-eldoc)
 ;;     (add-hook 'go-mode-hook 'go-eldoc-setup)
