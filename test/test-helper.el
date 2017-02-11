a2977ad5f16d17a63171f089cd8193851ff119f6
Syohei YOSHIDA <syohex@gmail.com>
2017-02-12 00:19:17 +0900

    Update copyright year

--- test/test-helper.el
+++ test/test-helper.el
@@ -1,6 +1,6 @@
 ;;; test-helper.el --- helper for testing go-eldoc
 
-;; Copyright (C) 2016 by Syohei YOSHIDA
+;; Copyright (C) 2017 by Syohei YOSHIDA
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 
